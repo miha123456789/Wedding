8cbc1048902289ca1bfa0819ae870e56a92a5031
miha123456789 <muhailo.makar@gmail.com>
2023-05-21 10:44:16 +0300

    AI(fixed)

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,59 +1,70 @@
 import { Component } from '@angular/core';
-import { MatCalendarCellCssClasses} from "@angular/material/datepicker";
+import { MatCalendarCellCssClasses } from "@angular/material/datepicker";
 import { ViewChild } from '@angular/core';
 import { MatCalendar } from '@angular/material/datepicker';
+
+interface BookedDate {
+  date: string;
+  name: string;
+  phone: string;
+  accept: boolean;
+}
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-
-
 export class CalendarComponent {
-  // @ts-ignore
-  @ViewChild(MatCalendar) calendar: MatCalendar<Date>;
+  @ViewChild(MatCalendar) calendar!: MatCalendar<Date>;
 
   name: string = '';
   phone: string = '';
   selected: Date = new Date();
+  bookedDates: BookedDate[] = JSON.parse(localStorage.getItem('bookedDates') || '[]');
   constructor() {}
 
-  // @ts-ignore
-  bookedDates: BookedDate[] = JSON.parse(localStorage.getItem('bookedDates')) || [];
   onSubmit() {
-     // @ts-ignore
-    const booking = {
-        date: this.selected.toUTCString(),
-        name: this.name,
-        phone: this.phone,
-        accept: false
-      };
-      // @ts-ignore
-      this.bookedDates.push(booking);
-     localStorage.setItem('bookedDates', JSON.stringify(this.bookedDates));
+    console.log(this.bookedDates);
+    const booking: BookedDate = {
+      date: this.selected.toISOString(),
+      name: this.name,
+      phone: this.phone,
+      accept: false
+    };
+
+    this.bookedDates.push(booking);
+    localStorage.setItem('bookedDates', JSON.stringify(this.bookedDates));
+
     this.name = '';
     this.phone = '';
     this.selected = new Date();
     this.calendar.updateTodaysDate();
   }
+
   dateClass(dates: Date): MatCalendarCellCssClasses {
     const classes: MatCalendarCellCssClasses = {};
-    let clas=false;
-    // @ts-ignore
-    JSON.parse(localStorage.getItem('bookedDates')).map((n)=>{
-      if(new Date(n.date).toISOString().slice(0, 10)===new Date(dates).toISOString().slice(0, 10)&&n.accept){
-        clas=true;
-      }
-    })
-    classes['available'] = clas;
+    const selectedDate = new Date(dates).toISOString().slice(0, 10);
+    let bookedDate;
+    const bookedDates: BookedDate[] = JSON.parse(localStorage.getItem('bookedDates') || '[]');
+
+    if (Array.isArray(bookedDates)) {
+      // @ts-ignore
+      bookedDate = bookedDates.find((date) => {
+        const bookedDate = new Date(date.date).toISOString().slice(0, 10);
+        return bookedDate === selectedDate && date.accept;
+      });
+    }
+    if (bookedDate) {
+      classes['available'] = true;
+    }
+
     return classes;
   }
 
-
-  onDateSelected(date: Date|null) {
-    if(date) {
+  onDateSelected(date: Date | null) {
+    if (date) {
       this.selected = date;
     }
   }
-
 }
